b2cc112354c9a71d679eb3724bbad765483ad441
Sameer Khan <samsam23122001@gmail.com>
2020-11-02 23:11:43 -0500

    Updated global variables

--- sportcred/lib/models/global.dart
+++ sportcred/lib/models/global.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
-Color sportGreen = new Color(0xFF51D16D);
-Color backgroundBlack = new Color(0xFF222629);
-Color backgroundGrey = Colors.grey[800];
+class AppTheme{
+  static final Color sportGreen = Color(0xFF51D16D);
+  static final Color backgroundBlack = Color(0xFF222629);
+  static final Color backgroundGray = Color(0xFF2b2b29);
+
+  static final TextStyle zoneText = new TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 30.0,
+    color: Colors.white,
+    fontWeight: FontWeight.w300
+  );
+
+}
