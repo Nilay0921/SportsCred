6ef158a3d12c789fbb34e3b35521fac033929ab7
William Lee <52926429+SSonagi@users.noreply.github.com>
2020-11-03 10:42:57 -0500

    [SYS-13] Connected the pages together

--- sportcred/lib/UI/LoginPage.dart
+++ sportcred/lib/UI/LoginPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sportcred/BottomNavigation.dart';
-import 'package:sportcred/models/global.dart';
+import 'package:sportcred/UI/RegisterPage.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,108 +8,123 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF2b2b29),
-        body: new Container(
-            padding: EdgeInsets.fromLTRB(30, 100, 30, 0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 180, 30),
-                  child: Text(
-                    'Login',
-                    textScaleFactor: 3,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: new Color.fromARGB(500, 500, 500, 500),
-                    ),
-                    textDirection: TextDirection.ltr,
-                    textAlign: TextAlign.left,
-                  ),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFF2b2b29),
+      body: new Container(
+        padding: EdgeInsets.fromLTRB(30, 100, 30, 0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 50),
+              child: Image(
+                image: new AssetImage('assets/images/Logo.png')
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
+              child: TextField(
+                obscureText: false,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(borderSide: new BorderSide(color: Colors.white)),
+                  focusedBorder: UnderlineInputBorder(borderSide: new BorderSide(color: Colors.white)),
                 ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  color: Colors.white,
-                  child: TextField(
-                    controller: emailController,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                    ),
-                  ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: TextField(
+                obscureText: true,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(borderSide: new BorderSide(color: Colors.white)),
+                  focusedBorder: UnderlineInputBorder(borderSide: new BorderSide(color: Colors.white)),
                 ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  color: Colors.white,
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                Container(
-                  child: RaisedButton(
-                    color: Colors.black,
-                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 129.0),
-                    //go to HomePage (The Zone)
-                    onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));},
-                    child: const Text(
-                      'Next',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('New to SportCred?'),
-                    TextButton(
-                      onPressed: () {
-//                    Navigator.push(
-//                      context,
-//                      MaterialPageRoute(
-//                        builder: (context) => RegisterPage(),
-//                      ),
-//                    );
-                      },
-                      child: Text(
-                        'Register',
-                        style: TextStyle(color: Color(0xff05FF00)),
-                      ),
-                    ),
-                  ],
-                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(177, 0, 0, 225),
+              child: TextButton(
+                onPressed: null,
+                child: Text('Forgot Password',
+                    style: TextStyle(color: Colors.grey)),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('New to SportCred?', style: TextStyle(color: Colors.white)),
                 TextButton(
-                  onPressed: null,
-                  child: Text('Forgot Password?',
-                      style: TextStyle(color: Colors.black)),
-                )
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RegisterPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Register',
+                    style: TextStyle(color: Color(0xff05FF00)),
+                  ),
+                ),
               ],
-            )));
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 0, bottom: 20),
+              child: RaisedButton(
+                color: Colors.transparent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                padding: EdgeInsets.all(0),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyHomePage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 7.0,
+                    horizontal: 125.0
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Color(0xff228B22),
+                        Color(0xff2EB62C),
+                        Color(0xff83D475)
+                      ],
+                    ),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      )
+    );
   }
 }
