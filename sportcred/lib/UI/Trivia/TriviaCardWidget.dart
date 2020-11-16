6289afc4b2bc61de1db11b6e62a50668ac3ba6f9
Naomi Victor <37161261+NaomiVictor@users.noreply.github.com>
2020-11-16 04:34:10 -0500

    [SYS-2] Updated trivia to have short answer questions & allow user to play against random/radar list opponents. Must connect to db

--- sportcred/lib/UI/Trivia/TriviaCardWidget.dart
+++ sportcred/lib/UI/Trivia/TriviaCardWidget.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:sportcred/models/global.dart';
 import 'package:sportcred/UI/Trivia/TriviaPage.dart';
+import 'package:sportcred/models/global.dart';
 
 class TriviaCardWidget extends StatelessWidget {
   final String title;
   final String img;
   final String connection;
-
-  const TriviaCardWidget(
-      {Key key, this.title, this.img, this.connection}) : super(key: key);
+  TriviaCardWidget({Key key, this.title, this.img, this.connection}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<String> radarOpponents = ['opponentA','opponentB','opponentC','opponentD','opponentE','opponentF','opponentG','opponentH','opponentI','opponentJ'];
+
+    List<ListTile> getOpponents() {
+      List<ListTile> opponents = [];
+      opponents.add(
+        ListTile(
+          leading: const Icon(Icons.cached_rounded),
+          title: Text('Random opponent', style: TextStyle(fontFamily: 'Lato')),
+          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => TriviaConnection('multi')))
+        )
+      );
+      for (var i in radarOpponents) {
+        opponents.add(
+            ListTile(
+                leading: const Icon(Icons.account_circle),
+              title: Text(i, style: TextStyle(fontFamily: 'Lato')),
+              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => TriviaConnection('multi')))
+            )
+        );
+      }
+      return opponents;
+    }
+
     return InkWell(
       onTap: () {
         if (connection == 'solo') {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => dbConnection()));
+          Navigator.push(context, MaterialPageRoute(builder: (context) => TriviaConnection('solo')));
+        } else if (connection == 'multi') {
+          showDialog<String>(context: context, builder: (BuildContext context) => SimpleDialog(
+              title: const Text('Choose opponent'),
+              titleTextStyle: TextStyle(
+                  fontFamily: 'Lato',
+                  color: Colors.green,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400),
+              children: getOpponents()
+            )
+          );
         }
       },
       child: Card(
@@ -34,7 +66,7 @@ class TriviaCardWidget extends StatelessWidget {
                     child: Padding(
                       padding: EdgeInsets.only(left: 10.0, top: 5.0),
                       child: Text(
-                          '$title',
+                          title,
                           textAlign: TextAlign.left,
                           style: AppTheme.triviaText
                       ),
@@ -45,7 +77,7 @@ class TriviaCardWidget extends StatelessWidget {
                             fit: BoxFit.cover,
                             alignment: FractionalOffset.topCenter,
                             colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.srcOver),
-                            image: new AssetImage('$img')
+                            image: new AssetImage(img)
                         )
                     ),
                   )
